0e5a643ab2f6f36e50b433294ff5adcc088fa433
Myroslava <milrusysurlim@gmail.com>
2025-06-02 14:27:34 +0300

    DbContext configured

--- VictoryCenter/VictoryCenter.DAL/Data/VictoryCenterDbContext.cs
+++ VictoryCenter/VictoryCenter.DAL/Data/VictoryCenterDbContext.cs
@@ -8,10 +8,24 @@ public class VictoryCenterDbContext : DbContext
     public VictoryCenterDbContext(DbContextOptions<VictoryCenterDbContext> options) 
         : base(options) {}
     
-    public DbSet<TestEntity> TestEntities { get; set; }
-    
+    public DbSet<Admin> Admins { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<TeamMember> TeamMembers { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TeamMember>()
+                .HasOne(t => t.Category)
+                .WithMany(c => c.TeamMembers)
+                .HasForeignKey(t => t.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<TeamMember>()
+            .HasIndex(t => new { t.CategoryId, t.Priority })
+            .IsUnique();
     }
 }
