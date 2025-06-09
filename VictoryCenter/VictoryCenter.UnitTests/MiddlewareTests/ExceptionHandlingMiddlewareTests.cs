4cc661486e75a6a6fee930fa7863a34bebe50a70
roman-stozhuk <Roman.Stozhuk.ShI.2023@lpnu.ua>
2025-06-10 00:45:54 +0300

    Updated ExceptionHandlingMiddlewareTests

--- VictoryCenter/VictoryCenter.UnitTests/MiddlewareTests/ExceptionHandlingMiddlewareTests.cs
+++ VictoryCenter/VictoryCenter.UnitTests/MiddlewareTests/ExceptionHandlingMiddlewareTests.cs
@@ -1,12 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-using System.Reflection;
 using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using Moq;
 using VictoryCenter.WebAPI.Middleware;
 
@@ -24,10 +20,11 @@ public class ExceptionHandlingMiddlewareTests
     }
 
     [Fact]
-    public async Task Invoke_NoException_ShouldCallNextWithoutLogging()
+    public async Task InvokeAsync_NoException_ShouldCallNextWithoutLoggingOrWritingResponse()
     {
         //Arrange
         var context = new DefaultHttpContext();
+        context.Response.Body = new MemoryStream();
         var nextCalled = false;
         var nextDelegate = new RequestDelegate(ctx =>
         {
@@ -42,28 +39,47 @@ public class ExceptionHandlingMiddlewareTests
         );
 
         // Act
-        await middleware.Invoke(context);
+        await middleware.InvokeAsync(context);
 
         //Assert
+        context.Response.Body.Seek(0, SeekOrigin.Begin);
+        var responceBody = new StreamReader(context.Response.Body).ReadToEnd();
+        Assert.True(string.IsNullOrEmpty(responceBody));
+        _loggerMock.VerifyNoOtherCalls();
+
         Assert.True(nextCalled);
-        _loggerMock.Verify(
-            m => m.Log(
-                LogLevel.Error,
-                It.IsAny<EventId>(),
-                It.IsAny<It.IsAnyType>(),
-                It.IsAny<Exception>(),
-                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
-            Times.Never
-        );
     }
 
     [Fact]
-    public async Task Invoke_WithException_ShouldLogException()
+    public async Task InvokeAsync_WithException_ShouldLogExceptionAndWriteProblemDetails()
     {
         // Arrange
+        var expectedMethod = "GET";
+        var expectedPath = "/test";
+
         var context = new DefaultHttpContext();
+        context.Request.Method = expectedMethod;
+        context.Request.Path = expectedPath;
         context.Response.Body = new MemoryStream();
-        var exception = new NotImplementedException();
+        var exception = new NotImplementedException("exception message");
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Internal Server Error",
+            Detail = "An error occurred while processing your request. Please try again!"
+        };
+
+        _factoryMock
+            .Setup(f => f.CreateProblemDetails(
+                context,
+                StatusCodes.Status500InternalServerError,
+                "Internal Server Error",
+                null,
+                "An error occurred while processing your request. Please try again!",
+                null
+            ))
+            .Returns(problemDetails);
 
         var middleware = new ExceptionHandlingMiddleware(
             _ => throw exception,
@@ -72,80 +88,44 @@ public class ExceptionHandlingMiddlewareTests
         );
 
         // Act
-        await middleware.Invoke(context);
+        await middleware.InvokeAsync(context);
 
         // Assert
-        Assert.Equal(501, context.Response.StatusCode);
-
         _loggerMock.Verify(
-            m => m.Log(
-                LogLevel.Error,
+            x => x.Log(
+                It.Is<LogLevel>(logLevel => logLevel == LogLevel.Critical),
                 It.IsAny<EventId>(),
                 It.IsAny<It.IsAnyType>(),
-                exception,
-                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
-            Times.Once);
-    }
-
-    [Theory]
-    [InlineData(typeof(ValidationException), 400, "Bad Request")]
-    [InlineData(typeof(UnauthorizedAccessException), 401, "Unauthorized")]
-    [InlineData(typeof(SecurityTokenException), 403, "Forbidden")]
-    [InlineData(typeof(KeyNotFoundException), 404, "Not Found")]
-    [InlineData(typeof(DbUpdateException), 409, "Conflict")]
-    [InlineData(typeof(NotImplementedException), 501, "Not Implemented")]
-    [InlineData(typeof(Exception), 500, "Internal Server Error")]
-    public async Task HandleExceptionAsync_SetsCorrectStatusAndProblemDetails(
-    Type exceptionType, int expectedStatusCode, string expectedTitle)
-    {
-        // Arrange
-        var context = new DefaultHttpContext();
-        context.Response.Body = new MemoryStream();
-        context.Request.Path = "/test";
-
-        var exception = (Exception)Activator.CreateInstance(exceptionType);
-
-        var expectedProblemDetails = new ProblemDetails
-        {
-            Status = expectedStatusCode,
-            Title = expectedTitle,
-            Detail = exception.Message,
-            Type = "test",
-            Instance = "/test"
-        };
-
-        _factoryMock.Setup(f => f.CreateProblemDetails(
-                context,
-                expectedStatusCode,
-                expectedTitle,
-                It.IsAny<string>(),
-                exception.Message,
-                "/test"
-            )).Returns(expectedProblemDetails);
-
-        var middleware = new ExceptionHandlingMiddleware(
-            _ => Task.CompletedTask,
-            _loggerMock.Object,
-            _factoryMock.Object);
-
-        var method = typeof(ExceptionHandlingMiddleware)
-            .GetMethod("HandleExceptionAsync", BindingFlags.NonPublic | BindingFlags.Instance);
-
-        // Act
-        await (Task)method.Invoke(middleware, [context, exception]);
-
-        context.Response.Body.Seek(0, SeekOrigin.Begin);
-        var responseJson = await new StreamReader(context.Response.Body).ReadToEndAsync();
-
-        var result = JsonSerializer.Deserialize<ProblemDetails>(
-            responseJson// , new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+                It.IsAny<NotImplementedException>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()
+            ),
+            Times.Once
         );
 
-        // Assert
-        Assert.Equal(expectedStatusCode, context.Response.StatusCode);
+        _factoryMock.Verify(
+            f => f.CreateProblemDetails(
+                context,
+                StatusCodes.Status500InternalServerError,
+                "Internal Server Error",
+                null,
+                "An error occurred while processing your request. Please try again!",
+                null
+            ),
+            Times.Once
+        );
+
         Assert.Equal("application/problem+json", context.Response.ContentType);
-        Assert.Equal(expectedProblemDetails.Title, result!.Title);
-        Assert.Equal(expectedProblemDetails.Type, result.Type);
-        Assert.Equal(expectedProblemDetails.Detail, result.Detail);
+        Assert.Equal(500, context.Response.StatusCode);
+
+        context.Response.Body.Seek(0, SeekOrigin.Begin);
+        var responseBody = await new StreamReader(context.Response.Body).ReadToEndAsync();
+        var deserializedBody = JsonSerializer.Deserialize<ProblemDetails>(responseBody);
+
+        Assert.NotNull(deserializedBody);
+        Assert.Equal(problemDetails.Status, deserializedBody.Status);
+        Assert.Equal(problemDetails.Title, deserializedBody.Title);
+        Assert.Equal(problemDetails.Detail, deserializedBody.Detail);
+        Assert.Null(problemDetails.Instance);
+        Assert.Null(problemDetails.Type);
     }
 }
