c68ecc40ba8fda5cda5f40227158bd5e12fcd6c0
Markiyan Bevz <bevzm50@gmail.com>
2025-06-11 00:19:26 +0300

    Fixed some bugs

--- VictoryCenter/VictoryCenter.BLL/Commands/TeamMember/DeleteTeamMemberCommand.cs
+++ VictoryCenter/VictoryCenter.BLL/Commands/TeamMember/DeleteTeamMemberCommand.cs
@@ -1,3 +1,6 @@
+using FluentResults;
+using MediatR;
+
 namespace VictoryCenter.BLL.Commands.TeamMember.DeleteTeamMember;
 
 public record DeleteTeamMemberCommand(int Id) : IRequest<Result<Unit>>;
--- VictoryCenter/VictoryCenter.BLL/Commands/TeamMember/DeleteTeamMemberHandler.cs
+++ VictoryCenter/VictoryCenter.BLL/Commands/TeamMember/DeleteTeamMemberHandler.cs
@@ -1,6 +1,6 @@
+using FluentResults;
 using MediatR;
 using VictoryCenter.DAL.Repositories.Interfaces;
-using VictoryCenter.Shared;
 
 namespace VictoryCenter.BLL.Commands.TeamMember.DeleteTeamMember;
 
@@ -18,15 +18,15 @@ public class DeleteTeamMemberHandler : IRequestHandler<DeleteTeamMemberCommand, 
         var exists = await _teamMemberRepository.ExistsAsync(request.Id);
         if (!exists)
         {
-            return Result<Unit>.Failure($"Team member with ID {request.Id} not found.");
+            return Result.Fail<Unit>($"Team member with ID {request.Id} not found.");
         }
 
         var deleted = await _teamMemberRepository.DeleteAsync(request.Id);
         if (!deleted)
         {
-            return Result<Unit>.Failure("Failed to delete the team member.");
+            return Result.Fail<Unit>("Failed to delete the team member.");
         }
 
-        return Result<Unit>.Success(Unit.Value);
+        return Result.Ok(Unit.Value);
     }
 }
