50d313f1db624cb2bef380e58873cf5507b04796
Myroslava <milrusysurlim@gmail.com>
2025-06-13 13:30:11 +0300

    Added DbUpdate project

---
+++ VictoryCenter/VictoryCenter.DbUpdate/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using VictoryCenter.DAL.Data;
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Local";
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables("VICTORYCENTER_")
+    .Build();
+
+var connectionString = config.GetConnectionString("DefaultConnection");
+
+Console.WriteLine($"Using connection string: {connectionString}");
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .AddConsole()
+        .SetMinimumLevel(LogLevel.Information);
+});
+
+var optionsBuilder = new DbContextOptionsBuilder<VictoryCenterDbContext>();
+optionsBuilder
+    .UseSqlServer(connectionString)
+    .UseLoggerFactory(loggerFactory);
+
+using var context = new VictoryCenterDbContext(optionsBuilder.Options);
+
+context.Database.Migrate();
