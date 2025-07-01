55ebd634f59449e019cdff5678eb1f300616801c
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-07-01 16:51:00 +0300

    Add PublicCategoryWithTeamMembersDto and  PublicTeamMemberDto

---
+++ VictoryCenter/VictoryCenter.BLL/DTOs/Categories/PublicCategoryWithTeamMembersDto.cs
@@ -0,0 +1,11 @@
+﻿using VictoryCenter.BLL.DTOs.TeamMembers;
+
+namespace VictoryCenter.BLL.DTOs.Categories;
+
+public record PublicCategoryWithTeamMembersDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public List<PublicTeamMemberDto> Members { get; set; } = [];
+}
