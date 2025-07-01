bcc9c8c0cb4c06eba1df44b9524303bc307dadcc
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-07-01 17:33:50 +0300

    Add mapping to PublicCategoryWithTeamMembersDto and  PublicTeamMemberDto

--- VictoryCenter/VictoryCenter.BLL/DTOs/Categories/PublicCategoryWithTeamMembersDto.cs
+++ VictoryCenter/VictoryCenter.BLL/DTOs/Categories/PublicCategoryWithTeamMembersDto.cs
@@ -5,7 +5,7 @@ namespace VictoryCenter.BLL.DTOs.Categories;
 public record PublicCategoryWithTeamMembersDto
 {
     public long Id { get; set; }
-    public string Name { get; set; } = null!;
+    public string CategoryName { get; set; } = null!;
     public string? Description { get; set; }
-    public List<PublicTeamMemberDto> Members { get; set; } = [];
+    public List<PublicTeamMemberDto> TeamMembers { get; set; } = [];
 }
