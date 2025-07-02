25d48ae8b7e68d1ad71d27705b6e7f1681b2caba
Markiyan Bevz <bevzm50@gmail.com>
2025-07-02 13:50:37 +0300

    Added test image entity

---
+++ VictoryCenter/VictoryCenter.DAL/Entities/Image.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace VictoryCenter.DAL.Entities.Images;
+
+[Table("images", Schema = "media")]
+public class Image
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [NotMapped]
+    public string? Base64 { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string? BlobName { get; set; }
+
+    [Required]
+    [MaxLength(10)]
+    public string? MimeType { get; set; }
+
+    [Required]
+    public TeamMember? TeamMember { get; set; }
+}
