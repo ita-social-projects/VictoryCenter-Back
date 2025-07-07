9d9fa5149d4623b1d8b626c4ff4805babe241408
Kibysh Taras <taras.kibysh,shi.2023@lpnu.ua>
2025-07-07 10:19:49 +0300

    Add test functionality for check coverage

---
+++ VictoryCenter/VictoryCenter.UnitTests/TestFunctionTests/TestFunctionTests.cs
@@ -0,0 +1,24 @@
+﻿using VictoryCenter.BLL.tests;
+
+namespace VictoryCenter.UnitTests.TestFunctionTests;
+
+public class TestFunctionTests
+{
+    private readonly TestFunction _calc = new();
+
+    [Fact]
+    public void Add_Works() => Assert.Equal(5, _calc.Add(2, 3));
+
+    [Fact]
+    public void Subtract_Works() => Assert.Equal(1, _calc.Subtract(3, 2));
+
+    [Fact]
+    public void Multiply_Works() => Assert.Equal(6, _calc.Multiply(2, 3));
+
+    [Fact]
+    public void Divide_Works() => Assert.Equal(2, _calc.Divide(6, 3));
+
+    [Fact]
+    public void Divide_ByZero_Throws()
+        => Assert.Throws<DivideByZeroException>(() => _calc.Divide(1, 0));
+}
