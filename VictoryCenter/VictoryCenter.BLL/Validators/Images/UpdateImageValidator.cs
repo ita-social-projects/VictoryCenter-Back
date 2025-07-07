315af392869bf4833d991c21cde01f39240bdc36
Markiyan Bevz <bevzm50@gmail.com>
2025-07-07 16:49:13 +0300

    Validator has been temporary fixed

--- VictoryCenter/VictoryCenter.BLL/Validators/Images/UpdateImageValidator.cs
+++ VictoryCenter/VictoryCenter.BLL/Validators/Images/UpdateImageValidator.cs
@@ -1,20 +1,20 @@
 using FluentValidation;
 
 // using VictoryCenter.BLL.Commands.Images.Update;
-using VictoryCenter.BLL.DTOs.Images;
+using VictoryCenter.BLL.Commands.Images.Update;
 
 namespace VictoryCenter.BLL.Validators.Images;
 
-public class UpdateImageValidator : AbstractValidator<UpdateImageDTO>
+public class UpdateImageValidator : AbstractValidator<UpdateImageCommand>
 {
     private static readonly string[] AllowedMimeTypes = { "image/jpeg", "image/jpg", "image/png", "image/webp" };
     public UpdateImageValidator(BaseImageValidator baseImageValidator)
     {
-        _ = RuleFor(x => x.Base64)
+        _ = RuleFor(x => x.updateImageDto.Base64)
             .NotEmpty().WithMessage("Base64 content is required")
             .Must(IsValidBase64).WithMessage("Base64 content is invalid");
 
-        RuleFor(x => x.MimeType)
+        RuleFor(x => x.updateImageDto.MimeType)
             .NotEmpty().WithMessage("MimeType is required")
             .Must(mimeType => AllowedMimeTypes.Contains(mimeType))
             .WithMessage($"MimeType must be one of the following: {string.Join(", ", AllowedMimeTypes)}");
