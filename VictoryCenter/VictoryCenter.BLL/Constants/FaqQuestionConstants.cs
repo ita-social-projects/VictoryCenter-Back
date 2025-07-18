36d11d999ec115cb598de243c444bc4e875848e4
roman-stozhuk <Roman.Stozhuk.ShI.2023@lpnu.ua>
2025-07-18 18:02:15 +0300

    Added error constants

---
+++ VictoryCenter/VictoryCenter.BLL/Constants/FaqQuestionConstants.cs
@@ -0,0 +1,6 @@
+namespace VictoryCenter.BLL.Constants;
+
+public static class FaqQuestionConstants
+{
+    public static readonly string PageNotFoundOrContainsNoFaqQuestions = "Page not found or does not contain FAQ question";
+}
