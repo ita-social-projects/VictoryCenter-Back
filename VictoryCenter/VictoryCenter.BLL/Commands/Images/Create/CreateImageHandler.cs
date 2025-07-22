2e9ebffcd6dc590273190f27fd784140790bbd86
Markiyan Bevz <bevzm50@gmail.com>
2025-07-22 17:18:15 +0300

    Changed createImagehandler using transaction

--- VictoryCenter/VictoryCenter.BLL/Commands/Images/Create/CreateImageHandler.cs
+++ VictoryCenter/VictoryCenter.BLL/Commands/Images/Create/CreateImageHandler.cs
@@ -30,29 +30,50 @@ public class CreateImageHandler : IRequestHandler<CreateImageCommand, Result<Ima
         try
         {
             await _validator.ValidateAndThrowAsync(request, cancellationToken);
+
             var fileName = Guid.NewGuid().ToString().Replace("-", "");
+
+            using var transaction = _repositoryWrapper.BeginTransaction();
+
             try
             {
-                var fileNewName = await _blobService.SaveFileInStorageAsync(request.CreateImageDto.Base64, fileName, request.CreateImageDto.MimeType);
+                // Спочатку зберігаємо в базу даних
+                Image image = _mapper.Map<Image>(request.CreateImageDto);
+                image.BlobName = fileName;
+                image.CreatedAt = DateTime.UtcNow;
+
+                Image createdImage = await _repositoryWrapper.ImageRepository.CreateAsync(image);
+
+                if (await _repositoryWrapper.SaveChangesAsync() <= 0)
+                {
+                    return Result.Fail<ImageDTO>(ImageConstants.FailToSaveImageInDatabase);
+                }
+
+                // Пробуєм тут зберегти файл в blob storage
+                try
+                {
+                    await _blobService.SaveFileInStorageAsync(request.CreateImageDto.Base64, fileName, request.CreateImageDto.MimeType);
+                }
+                catch (Exception e)
+                {
+                    // Транзакція автоматично зробить rollback при dispose
+                    return Result.Fail<ImageDTO>(ImageConstants.FailToSaveImageInStorage);
+                }
+
+                // Якщо все пройшло успішно, комітимо транзакцію
+                transaction.Complete();
+
+                // Повертаємо результат з завантаженим Base64
+                var response = _mapper.Map<ImageDTO>(createdImage);
+                response.Base64 = await _blobService.FindFileInStorageAsBase64Async(response.BlobName, response.MimeType);
+
+                return Result.Ok(response);
             }
             catch (Exception e)
             {
+                // Транзакція автоматично зробить rollback при dispose
                 return Result.Fail<ImageDTO>(ImageConstants.FailToSaveImageInStorage);
             }
-
-            Image? image = _mapper.Map<Image>(request.CreateImageDto);
-            image.BlobName = fileName;
-            image.CreatedAt = DateTime.UtcNow;
-
-            Image result = await _repositoryWrapper.ImageRepository.CreateAsync(image);
-            if (await _repositoryWrapper.SaveChangesAsync() > 0)
-            {
-                var responce = _mapper.Map<ImageDTO>(result);
-                responce.Base64 = await _blobService.FindFileInStorageAsBase64Async(responce.BlobName, responce.MimeType);
-                return Result.Ok(responce);
-            }
-
-            return Result.Fail<ImageDTO>(ImageConstants.FailToSaveImageInDatabase);
         }
         catch (ValidationException vex)
         {
