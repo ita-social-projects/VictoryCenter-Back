9969ee46a5a7e4e3924da14266237cf34916b76a
roman-stozhuk <Roman.Stozhuk.ShI.2023@lpnu.ua>
2025-07-23 21:20:47 +0300

    Moved entity configurations

---
+++ VictoryCenter/VictoryCenter.DAL/Data/EntityTypeConfigurations/TeamMemberConfig.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VictoryCenter.DAL.Entities;
+
+namespace VictoryCenter.DAL.Data.EntityTypeConfigurations;
+
+public class TeamMemberConfig : IEntityTypeConfiguration<TeamMember>
+{
+    public void Configure(EntityTypeBuilder<TeamMember> entity)
+    {
+        entity
+            .HasKey(e => e.Id);
+
+        entity
+            .Property(e => e.Id)
+            .ValueGeneratedOnAdd();
+
+        entity
+            .Property(e => e.FullName)
+            .IsRequired();
+
+        entity
+            .Property(e => e.CategoryId)
+            .IsRequired();
+
+        entity
+            .Property(e => e.Priority)
+            .IsRequired();
+
+        entity
+            .Property(e => e.Status)
+            .IsRequired();
+
+        entity
+            .Property(e => e.Description);
+
+        entity
+            .Property(e => e.Photo);
+
+        entity
+            .Property(e => e.Email);
+
+        entity
+            .Property(e => e.CreatedAt)
+            .IsRequired();
+
+        entity
+            .HasIndex(e => new { e.CategoryId, e.Priority })
+            .IsUnique();
+    }
+}
