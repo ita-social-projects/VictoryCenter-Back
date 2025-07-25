5480d63f488bfcae94555012da00a80fcce3ce5f
Vladimir <sushinskyvladimir@gmail.com>
2025-07-25 17:01:16 +0300

    Added separate entity config files

---
+++ VictoryCenter/VictoryCenter.DAL/Data/EntityTypeConfigurations/ImageConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VictoryCenter.DAL.Entities;
+
+namespace VictoryCenter.DAL.Data.EntityTypeConfigurations;
+
+public class ImageConfig : IEntityTypeConfiguration<Image>
+{
+    public void Configure(EntityTypeBuilder<Image> entity)
+    {
+        entity.ToTable("Images", "media");
+
+        entity.HasKey(e => e.Id);
+
+        entity.Property(e => e.Id)
+            .ValueGeneratedOnAdd();
+
+        entity.Property(e => e.BlobName)
+            .IsRequired();
+
+        entity.Property(e => e.MimeType)
+            .IsRequired();
+
+        entity.Property(e => e.CreatedAt)
+            .IsRequired();
+    }
+}
