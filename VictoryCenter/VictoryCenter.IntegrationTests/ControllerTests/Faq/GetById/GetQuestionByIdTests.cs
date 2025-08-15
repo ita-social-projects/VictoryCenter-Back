7c64a51159db9083fc8e01e06ed95c2531d45d67
Vladimir <sushinskyvladimir@gmail.com>
2025-08-15 17:46:19 +0300

    Added faq delete, update, getbyid tests

---
+++ VictoryCenter/VictoryCenter.IntegrationTests/ControllerTests/Faq/GetById/GetQuestionByIdTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using VictoryCenter.BLL.DTOs.Admin.TeamMembers;
+using VictoryCenter.IntegrationTests.ControllerTests.DbFixture;
+using VictoryCenter.IntegrationTests.Utils;
+
+namespace VictoryCenter.IntegrationTests.ControllerTests.Faq.GetById;
+
+public class GetQuestionByIdTests : BaseTestClass
+{
+    public GetQuestionByIdTests(IntegrationTestDbFixture fixture)
+        : base(fixture)
+    {
+    }
+
+    [Fact]
+    public async Task GetFaqQuestionById_ShouldReturnOk()
+    {
+        // Arrange
+        var existingEntity = await Fixture.DbContext.FaqQuestions.FirstOrDefaultAsync()
+            ?? throw new InvalidOperationException("Couldn't setup existing entity");
+
+        // Act
+        var response = await Fixture.HttpClient.GetAsync(new Uri($"api/faq/{existingEntity!.Id}", UriKind.Relative));
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        var responseContent = JsonSerializer.Deserialize<TeamMemberDto>(responseString, JsonOptions);
+
+        // Assert
+        Assert.True(response.IsSuccessStatusCode);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(responseContent);
+    }
+
+    [Fact]
+    public async Task GetTeamMemberById_ShouldFail_NotFound()
+    {
+        var response = await Fixture.HttpClient.GetAsync(new Uri($"api/faq/{-1}", UriKind.Relative));
+
+        Assert.False(response.IsSuccessStatusCode);
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
---
+++ VictoryCenter/VictoryCenter.IntegrationTests/ControllerTests/Faq/Update/UpdateFaqQuestionTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using VictoryCenter.BLL.DTOs.Admin.FaqQuestions;
+using VictoryCenter.DAL.Entities;
+using VictoryCenter.DAL.Enums;
+using VictoryCenter.IntegrationTests.ControllerTests.DbFixture;
+using VictoryCenter.IntegrationTests.Utils;
+
+namespace VictoryCenter.IntegrationTests.ControllerTests.Faq.Update;
+
+public class UpdateFaqQuestionTests : BaseTestClass
+{
+    public UpdateFaqQuestionTests(IntegrationTestDbFixture fixture)
+        : base(fixture)
+    {
+    }
+
+    [Fact]
+    public async Task UpdateFaqQuestion_ValidRequest_ReturnsSuccess()
+    {
+        FaqQuestion existingEntity = await Fixture.DbContext.FaqQuestions.FirstOrDefaultAsync()
+            ?? throw new InvalidOperationException("No FaqQuestion entity exists in the database.");
+
+        var updateFaqQuestionDto = new UpdateFaqQuestionDto
+        {
+            QuestionText = "QQQQQQQQQQQQQQQQQQQQ",
+            AnswerText = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+            PageIds = [3],
+            Status = Status.Published,
+        };
+        var serializedDto = JsonSerializer.Serialize(updateFaqQuestionDto);
+
+        HttpResponseMessage response = await Fixture.HttpClient.PutAsync(new Uri($"/api/faq/{existingEntity.Id}", UriKind.Relative), new StringContent(
+                serializedDto, Encoding.UTF8, "application/json"));
+        var responseString = await response.Content.ReadAsStringAsync();
+        FaqQuestionDto? responseContent = JsonSerializer.Deserialize<FaqQuestionDto>(responseString, JsonOptions);
+
+        response.EnsureSuccessStatusCode();
+        Assert.NotNull(responseContent);
+    }
+
+    [Fact]
+    public async Task UpdateFaqQuestion_InvalidQuestionText_ReturnsBadRequest()
+    {
+        FaqQuestion existingEntity = await Fixture.DbContext.FaqQuestions.FirstOrDefaultAsync()
+            ?? throw new InvalidOperationException("No FaqQuestion entity exists in the database.");
+
+        var updateFaqQuestionDto = new UpdateFaqQuestionDto
+        {
+            QuestionText = "",
+            AnswerText = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+            PageIds = [3],
+            Status = Status.Published,
+        };
+        var serializedDto = JsonSerializer.Serialize(updateFaqQuestionDto);
+
+        HttpResponseMessage response = await Fixture.HttpClient.PutAsync(new Uri($"/api/faq/{existingEntity.Id}", UriKind.Relative), new StringContent(
+                serializedDto, Encoding.UTF8, "application/json"));
+
+        Assert.False(response.IsSuccessStatusCode);
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task UpdateFaqQuestion_InvalidId_ReturnsNotFound()
+    {
+        var updateFaqQuestionDto = new UpdateFaqQuestionDto
+        {
+            QuestionText = "QQQQQQQQQQQQQQQQQQQQ",
+            AnswerText = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+            PageIds = [3],
+            Status = Status.Published,
+        };
+        var serializedDto = JsonSerializer.Serialize(updateFaqQuestionDto);
+
+        HttpResponseMessage response = await Fixture.HttpClient.PutAsync(new Uri($"/api/faq/{-1}", UriKind.Relative), new StringContent(
+                serializedDto, Encoding.UTF8, "application/json"));
+
+        Assert.False(response.IsSuccessStatusCode);
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
