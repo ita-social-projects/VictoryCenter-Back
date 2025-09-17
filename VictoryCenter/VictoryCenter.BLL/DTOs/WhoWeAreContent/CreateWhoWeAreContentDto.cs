ebdea3283457142b028c67fc2bca7512d95fb003
Taras Kibysh <taras.kibysh.shi.2023@lpnu.ua>
2025-09-17 17:05:09 +0300

    change set to init in dto

--- VictoryCenter/VictoryCenter.BLL/DTOs/WhoWeAreContent/CreateWhoWeAreContentDto.cs
+++ VictoryCenter/VictoryCenter.BLL/DTOs/WhoWeAreContent/CreateWhoWeAreContentDto.cs
@@ -4,13 +4,13 @@ namespace VictoryCenter.BLL.DTOs.WhoWeAreContent;
 
 public class CreateWhoWeAreContentDto
 {
-    public ContentType ContentType { get; set; }
+    public ContentType ContentType { get; init; }
 
-    public long Id { get; set; }
+    public long Id { get; init; }
 
     public string? Title { get; init; }
 
     public string? Description { get; init; }
 
-    public long? ImageId { get; set; }
+    public long? ImageId { get; init; }
 }
