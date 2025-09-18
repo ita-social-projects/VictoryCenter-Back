632758626fbc77d28ccdc04fa6433dd505980fc9
Motruk Oleg <oleh.motruk.shi.2023@lpnu.ua>
2025-09-18 12:48:34 +0300

    add validation on team member full name, that name can consist only letters , spaces , apostrophe

--- VictoryCenter/VictoryCenter.BLL/Validators/TeamMembers/CreateTeamMemberValidator.cs
+++ VictoryCenter/VictoryCenter.BLL/Validators/TeamMembers/CreateTeamMemberValidator.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
 using VictoryCenter.BLL.Commands.Admin.TeamMembers.Create;
+using VictoryCenter.BLL.Constants;
+using VictoryCenter.BLL.DTOs.Admin.TeamMembers;
 
 namespace VictoryCenter.BLL.Validators.TeamMembers;
 
@@ -8,5 +10,8 @@ public class CreateTeamMemberValidator : AbstractValidator<CreateTeamMemberComma
     public CreateTeamMemberValidator(BaseTeamMembersValidator baseTeamMembersValidator)
     {
         RuleFor(c => c.CreateTeamMemberDto).SetValidator(baseTeamMembersValidator);
+        RuleFor(c => c.CreateTeamMemberDto.FullName)
+                    .Matches(@"^[\p{L}'\- ]+$")
+                    .WithMessage(ErrorMessagesConstants.PropertyMustBeInAValidFormat(nameof(CreateTeamMemberDto.FullName)));
     }
 }
