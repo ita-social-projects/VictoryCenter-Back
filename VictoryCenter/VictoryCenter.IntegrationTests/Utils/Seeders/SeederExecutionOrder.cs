bd9f25862c1d539a6f58a17698c957e27814e1ac
Motruk Oleg <oleh.motruk.shi.2023@lpnu.ua>
2025-10-15 22:39:32 +0300

    fix conflicts with release

--- VictoryCenter/VictoryCenter.IntegrationTests/Utils/Seeders/SeederExecutionOrder.cs
+++ VictoryCenter/VictoryCenter.IntegrationTests/Utils/Seeders/SeederExecutionOrder.cs
@@ -7,7 +7,7 @@ public enum SeederExecutionOrder
     TeamMembers,
     HippotherapyProgramCategories,
     HippotherapyPrograms,
-    Images
+    Images,
     UahBankDetails,
     SupportOptions,
     ForeignBankDetails,
