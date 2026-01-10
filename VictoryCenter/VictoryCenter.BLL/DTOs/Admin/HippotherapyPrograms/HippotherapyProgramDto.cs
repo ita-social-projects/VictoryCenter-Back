cecf083cb249a007fcc2129100e97888d5dc9adb
denys-pavskyi <den.pavski@gmail.com>
2026-01-10 20:46:06 +0200

    feature: Added missing slug field to other Dtos, added Slugify library

--- VictoryCenter/VictoryCenter.BLL/DTOs/Admin/HippotherapyPrograms/HippotherapyProgramDto.cs
+++ VictoryCenter/VictoryCenter.BLL/DTOs/Admin/HippotherapyPrograms/HippotherapyProgramDto.cs
@@ -8,6 +8,7 @@ public record HippotherapyProgramDto
 {
     public long Id { get; init; }
     public string Name { get; init; } = null!;
+    public string Slug { get; init; } = null!;
     public string? Description { get; init; }
     public Status Status { get; init; }
     public string? Location { get; init; }
