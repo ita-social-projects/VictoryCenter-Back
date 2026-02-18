1fd5eac5147db8981bd56a13e999c8f48b714f68
VolodymyrS <volodymyr.sh.05@gmail.com>
2026-02-18 18:49:17 +0200

    feat: add localizations property to CategoryWithPublishedTeamMembersDto

--- VictoryCenter/VictoryCenter.BLL/DTOs/Public/TeamPage/CategoryWithPublishedTeamMembersDto.cs
+++ VictoryCenter/VictoryCenter.BLL/DTOs/Public/TeamPage/CategoryWithPublishedTeamMembersDto.cs
@@ -1,3 +1,5 @@
+using VictoryCenter.BLL.DTOs.Admin.Localization.TeamCategories;
+
 namespace VictoryCenter.BLL.DTOs.Public.TeamPage;
 
 public record CategoryWithPublishedTeamMembersDto
@@ -5,5 +7,6 @@ public record CategoryWithPublishedTeamMembersDto
     public long Id { get; init; }
     public string CategoryName { get; init; } = null!;
     public string? Description { get; init; }
+    public List<TeamCategoryLocalizationDto> Localizations { get; init; } = [];
     public List<PublishedTeamMembersDto> TeamMembers { get; init; } = [];
 }
