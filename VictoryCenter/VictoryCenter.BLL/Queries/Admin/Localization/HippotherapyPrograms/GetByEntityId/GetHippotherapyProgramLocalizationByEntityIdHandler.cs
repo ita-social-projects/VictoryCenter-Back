5bd11dededa71d706f61e5488d863b8ff1962a8d
Oleh Ponziuk <kuznicovoleh4@icloud.com>
2026-02-20 14:21:36 +0200

    Fix get by id handler

--- VictoryCenter/VictoryCenter.BLL/Queries/Admin/Localization/HippotherapyPrograms/GetByEntityId/GetHippotherapyProgramLocalizationByEntityIdHandler.cs
+++ VictoryCenter/VictoryCenter.BLL/Queries/Admin/Localization/HippotherapyPrograms/GetByEntityId/GetHippotherapyProgramLocalizationByEntityIdHandler.cs
@@ -1,6 +1,25 @@
+using AutoMapper;
+using FluentResults;
+using MediatR;
+using VictoryCenter.BLL.DTOs.Admin.Localization.HippotherapyProgram;
+using VictoryCenter.DAL.Repositories.Interfaces.Base;
+using VictoryCenter.DAL.Repositories.Options;
+
 namespace VictoryCenter.BLL.Queries.Admin.Localization.HippotherapyPrograms.GetByEntityId;
 
-public class GetHippotherapyProgramLocalizationByEntityIdHandler
+public class GetHippotherapyProgramLocalizationByEntityIdHandler : IRequestHandler<GetHippotherapyProgramLocalizationByEntityIdQuery, Result<IEnumerable<HippotherapyProgramLocalizationDto>>>
 {
-    
+    private readonly IRepositoryWrapper _wrapper;
+    private readonly IMapper _mapper;
+
+    public GetHippotherapyProgramLocalizationByEntityIdHandler(IRepositoryWrapper wrapper, IMapper mapper)
+    {
+        _wrapper = wrapper;
+        _mapper = mapper;
+    }
+
+    public Task<Result<IEnumerable<HippotherapyProgramLocalizationDto>>> Handle(GetHippotherapyProgramLocalizationByEntityIdQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
