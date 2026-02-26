679b1cca5e835912103451900e5a3357c07bab8b
Vektor19 <viktor.komissarov@knu.ua>
2026-02-26 17:58:38 +0200

    feat: aggregate translation statuses for each WhoWeAreSection based on all contents of the section

--- VictoryCenter/VictoryCenter.BLL/DTOs/Admin/WhoWeAreSection/WhoWeAreSectionInfoDto.cs
+++ VictoryCenter/VictoryCenter.BLL/DTOs/Admin/WhoWeAreSection/WhoWeAreSectionInfoDto.cs
@@ -1,9 +1,12 @@
+using VictoryCenter.BLL.DTOs.Admin.Localization.Common;
+
 namespace VictoryCenter.BLL.DTOs.Admin.WhoWeAreSection;
 
 public class WhoWeAreSectionInfoDto
 {
     public long Id { get; set; }
     public string SectionType { get; set; } = null!;
+    public List<TranslationStatusInfoDto> TranslationStatuses { get; set; } = [];
 
     public required string Title { get; set; }
 }
