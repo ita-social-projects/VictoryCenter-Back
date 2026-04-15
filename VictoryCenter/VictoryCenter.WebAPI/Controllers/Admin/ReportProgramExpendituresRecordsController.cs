28fdf8b1cf0e4393467ff51bd0296a96b0f00cf5
pichus <dimatyzuk@gmail.com>
2026-04-15 23:16:20 +0300

    feat: implement single ReportProgramExpendituresRecord deletion functionality

--- VictoryCenter/VictoryCenter.WebAPI/Controllers/Admin/ReportProgramExpendituresRecordsController.cs
+++ VictoryCenter/VictoryCenter.WebAPI/Controllers/Admin/ReportProgramExpendituresRecordsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using VictoryCenter.BLL.Commands.Admin.ReportProgramExpendituresRecords.Create;
+using VictoryCenter.BLL.Commands.Admin.ReportProgramExpendituresRecords.Delete;
 using VictoryCenter.BLL.DTOs.Admin.ReportProgramExpendituresRecords;
 using VictoryCenter.WebAPI.Controllers.Common;
 
@@ -16,5 +17,14 @@ public class ReportProgramExpendituresRecordsController : AuthorizedApiControlle
     {
         return HandleResult(await Mediator.Send(
             new CreateReportProgramExpendituresRecordCommand(createReportProgramExpendituresRecordDto)));
+    }
+
+    [HttpDelete("{id:long}")]
+    [ProducesResponseType(typeof(long), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> DeleteReportProgramExpenditureRecordAsync([FromRoute] long id)
+    {
+        return HandleResult(await Mediator.Send(new DeleteReportProgramExpendituresRecordCommand(id)));
     }
 }
