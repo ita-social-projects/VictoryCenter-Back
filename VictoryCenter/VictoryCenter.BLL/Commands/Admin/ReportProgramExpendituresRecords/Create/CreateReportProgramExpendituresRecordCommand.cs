ac931eb7ead05d51e41d71b5aec9fc5ac2928a65
pichus <dimatyzuk@gmail.com>
2026-04-20 18:24:25 +0300

    refactor: remove manual validation in CreateReportProgramExpendituresRecordHandler to demonstrate the usage of the new validation behavior

--- VictoryCenter/VictoryCenter.BLL/Commands/Admin/ReportProgramExpendituresRecords/Create/CreateReportProgramExpendituresRecordCommand.cs
+++ VictoryCenter/VictoryCenter.BLL/Commands/Admin/ReportProgramExpendituresRecords/Create/CreateReportProgramExpendituresRecordCommand.cs
@@ -1,9 +1,10 @@
 using FluentResults;
 using MediatR;
+using VictoryCenter.BLL.Behaviors.Abstractions;
 using VictoryCenter.BLL.DTOs.Admin.ReportProgramExpendituresRecords;
 
 namespace VictoryCenter.BLL.Commands.Admin.ReportProgramExpendituresRecords.Create;
 
 public record CreateReportProgramExpendituresRecordCommand(
     CreateReportProgramExpendituresRecordDto CreateReportProgramExpendituresRecordDto)
-    : IRequest<Result<ReportProgramExpendituresRecordDto>>;
+    : IRequest<Result<ReportProgramExpendituresRecordDto>>, IValidatableRequest;
