2880fe9a5712d57512dfec812858f3fc7634d338
Valentyn Humen <valentin34561@gmail.com>
2026-05-08 13:38:59 +0300

    fix error

--- VictoryCenter/VictoryCenter.BLL/DTOs/Admin/Localization/History/Create/IHistoryContentLocalization.cs
+++
@@ -1,8 +0,0 @@
-namespace VictoryCenter.BLL.DTOs.Admin.Localization.History.Create;
-
-public interface IHistoryContentLocalization
-{
-    long EntityId { get; }
-    string? Title { get; }
-    string? Description { get; }
-}
