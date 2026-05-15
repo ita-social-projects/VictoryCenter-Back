9e162bc65702a9eefb7e616a33ca4b7265f943e6
Shribak Bohdan <shribakb@gmail.com>
2026-05-15 10:06:46 +0300

    feature: create migration

---
+++ VictoryCenter/VictoryCenter.DAL/Migrations/20260515061809_AddReportFundsExpendituresSettingsLocalization.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VictoryCenter.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReportFundsExpendituresSettingsLocalization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReportFundsExpendituresSettingsLocalizations",
+                columns: table => new
+                {
+                    EntityId = table.Column<long>(type: "bigint", nullable: false),
+                    LanguageId = table.Column<long>(type: "bigint", nullable: false),
+                    DisclaimerTitle = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    TranslationStatus = table.Column<int>(type: "int", nullable: false, defaultValue: 1),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportFundsExpendituresSettingsLocalizations", x => new { x.EntityId, x.LanguageId });
+                    table.ForeignKey(
+                        name: "FK_ReportFundsExpendituresSettingsLocalizations_LocalizationLanguages_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "LocalizationLanguages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ReportFundsExpendituresSettingsLocalizations_ReportFundsExpendituresSettings_EntityId",
+                        column: x => x.EntityId,
+                        principalTable: "ReportFundsExpendituresSettings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReportFundsExpendituresSettingsLocalizations_LanguageId",
+                table: "ReportFundsExpendituresSettingsLocalizations",
+                column: "LanguageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReportFundsExpendituresSettingsLocalizations");
+        }
+    }
+}
