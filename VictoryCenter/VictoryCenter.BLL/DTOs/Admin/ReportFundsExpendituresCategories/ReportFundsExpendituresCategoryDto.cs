1fdd7452342913f5a5e2bba60f3744c8bc43ff59
Shribak Bohdan <shribakb@gmail.com>
2026-05-19 06:56:09 +0300

    refactor/issue-2107: fix bug when updating existing category throws error

--- VictoryCenter/VictoryCenter.BLL/DTOs/Admin/ReportFundsExpendituresCategories/ReportFundsExpendituresCategoryDto.cs
+++ VictoryCenter/VictoryCenter.BLL/DTOs/Admin/ReportFundsExpendituresCategories/ReportFundsExpendituresCategoryDto.cs
@@ -1,3 +1,4 @@
+using VictoryCenter.BLL.DTOs.Admin.Localization.ReportFundsExpendituresCategories;
 using VictoryCenter.DAL.Enums;
 
 namespace VictoryCenter.BLL.DTOs.Admin.ReportFundsExpendituresCategories;
@@ -7,4 +8,5 @@ public record ReportFundsExpendituresCategoryDto
     public long Id { get; init; }
     public string Name { get; init; } = null!;
     public ReportFundsExpendituresType Type { get; init; }
+    public IEnumerable<ReportFundsExpendituresCategoryLocalizationDto> Localizations { get; init; } = [];
 }
--- VictoryCenter/VictoryCenter.IntegrationTests/ControllerTests/ReportFundsExpendituresCategories/GetAll/GetAllReportFundsExpendituresCategoriesTests.cs
+++ VictoryCenter/VictoryCenter.IntegrationTests/ControllerTests/ReportFundsExpendituresCategories/GetAll/GetAllReportFundsExpendituresCategoriesTests.cs
@@ -1,6 +1,7 @@
 using Newtonsoft.Json;
 using VictoryCenter.BLL.DTOs.Admin.ReportFundsExpendituresCategories;
 using VictoryCenter.DAL.Entities;
+using VictoryCenter.DAL.Entities.Localization;
 using VictoryCenter.DAL.Enums;
 using VictoryCenter.IntegrationTests.Utils;
 using VictoryCenter.IntegrationTests.Utils.DbFixture;
@@ -34,5 +35,41 @@ public class GetAllReportFundsExpendituresCategoriesTests : BaseTestClass
 
         Assert.NotNull(responseContent);
         Assert.NotEmpty(responseContent);
+    }
+
+    [Fact]
+    public async Task GetAllCategories_ShouldIncludeLocalizations()
+    {
+        var category = new ReportFundsExpendituresCategory
+        {
+            Name = "Category with localization",
+            Type = ReportFundsExpendituresType.Income,
+            CreatedAt = DateTimeOffset.UtcNow
+        };
+        await Fixture.DbContext.ReportFundsExpendituresCategories.AddAsync(category);
+        await Fixture.DbContext.SaveChangesAsync();
+
+        await Fixture.DbContext.ReportFundsExpendituresCategoryLocalizations.AddAsync(
+            new ReportFundsExpendituresCategoryLocalization
+            {
+                EntityId = category.Id,
+                LanguageId = 2,
+                Name = "Category EN",
+                TranslationStatus = TranslationStatus.Relevant,
+                CreatedAt = DateTimeOffset.UtcNow
+            });
+        await Fixture.DbContext.SaveChangesAsync();
+
+        HttpResponseMessage response = await Fixture.HttpClient.GetAsync("/api/ReportFundsExpendituresCategories/");
+        response.EnsureSuccessStatusCode();
+
+        var responseString = await response.Content.ReadAsStringAsync();
+        IEnumerable<ReportFundsExpendituresCategoryDto>? responseContent =
+            JsonConvert.DeserializeObject<IEnumerable<ReportFundsExpendituresCategoryDto>>(responseString);
+
+        Assert.NotNull(responseContent);
+        var seeded = responseContent.FirstOrDefault(c => c.Id == category.Id);
+        Assert.NotNull(seeded);
+        Assert.NotEmpty(seeded.Localizations);
     }
 }
