1906feb6c439fe2b3c5826510e99bfcaf3100a22
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2026-08-09 17:12:44 +0300

    [Events] Implement admin event news update API

--- VictoryCenter/VictoryCenter.WebAPI/Controllers/Admin/EventNewsController.cs
+++ VictoryCenter/VictoryCenter.WebAPI/Controllers/Admin/EventNewsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using VictoryCenter.BLL.Commands.Admin.EventNews.Create;
+using VictoryCenter.BLL.Commands.Admin.EventNews.Update;
 using VictoryCenter.BLL.DTOs.Admin.EventNews;
 using VictoryCenter.WebAPI.Controllers.Common;
 
@@ -11,5 +12,15 @@ public class EventNewsController : AuthorizedApiController
     public async Task<IActionResult> CreateEventNews([FromBody] CreateEventNewsDto createEventNewsDto)
     {
         return HandleResult(await Mediator.Send(new CreateEventNewsCommand(createEventNewsDto)));
+    }
+
+    [HttpPut("{id:long}")]
+    [ProducesResponseType(typeof(EventNewsDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdateEventNews(long id, [FromBody] UpdateEventNewsDto updateEventNewsDto)
+    {
+        return HandleResult(await Mediator.Send(new UpdateEventNewsCommand(id, updateEventNewsDto)));
     }
 }
