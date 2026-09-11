c003a27558ff9cb1607970e60a505743316d5345
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2026-09-11 10:04:47 +0300

    feat: rate limit admin login attempts

--- VictoryCenter/VictoryCenter.WebAPI/Controllers/Admin/AuthController.cs
+++ VictoryCenter/VictoryCenter.WebAPI/Controllers/Admin/AuthController.cs
@@ -1,19 +1,23 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 using VictoryCenter.BLL.Constants;
 using VictoryCenter.BLL.Commands.Admin.Auth.Login;
 using VictoryCenter.BLL.Commands.Admin.Auth.Logout;
 using VictoryCenter.BLL.Commands.Admin.Auth.RefreshToken;
 using VictoryCenter.BLL.DTOs.Admin.Auth;
 using VictoryCenter.WebAPI.Controllers.Common;
+using VictoryCenter.WebAPI.Extensions;
 
 namespace VictoryCenter.WebAPI.Controllers.Admin;
 
 public class AuthController : BaseApiController
 {
     [HttpPost("login")]
+    [EnableRateLimiting(RateLimitingPolicyNameConstants.AdminLogin)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status429TooManyRequests)]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AuthResponseDto))]
     public async Task<IActionResult> LoginAsync(LoginRequestDto requestDto)
     {
