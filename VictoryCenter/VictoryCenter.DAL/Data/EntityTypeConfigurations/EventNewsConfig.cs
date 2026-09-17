b605b1ea1d7d56b03632853757d9fe80e14073f6
= <=>
2026-09-17 18:19:12 +0300

    add: title and description

--- VictoryCenter/VictoryCenter.DAL/Data/EntityTypeConfigurations/EventNewsConfig.cs
+++ VictoryCenter/VictoryCenter.DAL/Data/EntityTypeConfigurations/EventNewsConfig.cs
@@ -22,6 +22,14 @@ public class EventNewsConfig : IEntityTypeConfiguration<EventNews>
 
         builder.Property(e => e.Resource);
 
+        builder.Property(e => e.Title)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(e => e.Description)
+            .IsRequired()
+            .HasMaxLength(200);
+
         builder.Property(e => e.PublishedAt);
 
         builder.Property(e => e.Status)
