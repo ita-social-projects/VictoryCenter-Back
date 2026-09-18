0e30b5025aca1ec56f80ecde1401067579a86dde
= <=>
2026-09-18 10:48:38 +0300

    fix: add title and description validations and tests

--- VictoryCenter/VictoryCenter.BLL/Validators/EventNews/BaseEventNewsValidator.cs
+++ VictoryCenter/VictoryCenter.BLL/Validators/EventNews/BaseEventNewsValidator.cs
@@ -20,6 +20,32 @@ public class BaseEventNewsValidator : AbstractValidator<CreateEventNewsDto>
                 EventNewsConstants.ResourceMaxLength))
             .When(dto => !string.IsNullOrWhiteSpace(dto.Resource));
 
+        RuleFor(dto => dto.Title)
+            .NotEmpty()
+            .WithMessage(ErrorMessagesConstants.PropertyIsRequired(
+                nameof(CreateEventNewsDto.Title)))
+            .MinimumLength(EventNewsConstants.TitleMinLength)
+            .WithMessage(ErrorMessagesConstants.PropertyMustHaveAMinimumLengthOfNCharacters(
+                nameof(CreateEventNewsDto.Title), EventNewsConstants.TitleMinLength))
+            .MaximumLength(EventNewsConstants.TitleMaxLength)
+            .WithMessage(ErrorMessagesConstants.PropertyMustHaveAMaximumLengthOfNCharacters(
+                nameof(CreateEventNewsDto.Title), EventNewsConstants.TitleMaxLength))
+            .When(dto => dto.Status == Status.Published);
+
+        RuleFor(dto => dto.Description)
+            .NotEmpty()
+            .WithMessage(ErrorMessagesConstants.PropertyIsRequired(
+                nameof(CreateEventNewsDto.Description)))
+            .MinimumLength(EventNewsConstants.DescriptionMinLength)
+            .WithMessage(ErrorMessagesConstants.PropertyMustHaveAMinimumLengthOfNCharacters(
+                nameof(CreateEventNewsDto.Description),
+                EventNewsConstants.DescriptionMinLength))
+            .MaximumLength(EventNewsConstants.DescriptionMaxLength)
+            .WithMessage(ErrorMessagesConstants.PropertyMustHaveAMaximumLengthOfNCharacters(
+                nameof(CreateEventNewsDto.Description),
+                EventNewsConstants.DescriptionMaxLength))
+            .When(dto => dto.Status == Status.Published);
+
         RuleFor(dto => dto.PublishedAt)
             .NotNull()
             .WithMessage(ErrorMessagesConstants.PropertyIsRequired(nameof(CreateEventNewsDto.PublishedAt)))
