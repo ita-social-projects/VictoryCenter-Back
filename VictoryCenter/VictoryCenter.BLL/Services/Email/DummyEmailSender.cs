d6e4c2682ec398d34edf4037ac319483058fca52
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2026-09-23 09:15:58 +0300

    fix(email): remove unnecessary dummy delivery log

--- VictoryCenter/VictoryCenter.BLL/Services/Email/DummyEmailSender.cs
+++ VictoryCenter/VictoryCenter.BLL/Services/Email/DummyEmailSender.cs
@@ -1,5 +1,4 @@
 using FluentResults;
-using Microsoft.Extensions.Logging;
 using VictoryCenter.BLL.DTOs.Common;
 using VictoryCenter.BLL.Interfaces.Email;
 
@@ -7,17 +6,8 @@ namespace VictoryCenter.BLL.Services.Email;
 
 public class DummyEmailSender : IEmailSender
 {
-    private readonly ILogger<DummyEmailSender> _logger;
-
-    public DummyEmailSender(ILogger<DummyEmailSender> logger)
-    {
-        _logger = logger;
-    }
-
     public Task<Result> SendEmailAsync(EmailDto emailToSend)
     {
-        _logger.LogInformation("A dummy email was handled without external delivery.");
-
         return Task.FromResult(Result.Ok());
     }
 }
--- VictoryCenter/VictoryCenter.UnitTests/ServiceTests/Email/DummyEmailSenderTests.cs
+++ VictoryCenter/VictoryCenter.UnitTests/ServiceTests/Email/DummyEmailSenderTests.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.Logging;
-using Moq;
 using VictoryCenter.BLL.DTOs.Common;
 using VictoryCenter.BLL.Services.Email;
 
@@ -7,27 +5,15 @@ namespace VictoryCenter.UnitTests.ServiceTests.Email;
 
 public class DummyEmailSenderTests
 {
-    private readonly Mock<ILogger<DummyEmailSender>> _loggerMock = new();
-
     [Fact]
-    public async Task SendEmailAsync_ShouldReturnSuccessWithoutLoggingEmailContent()
+    public async Task SendEmailAsync_ShouldReturnSuccess()
     {
         var email = CreateEmail();
-        var sender = new DummyEmailSender(_loggerMock.Object);
+        var sender = new DummyEmailSender();
 
         var result = await sender.SendEmailAsync(email);
 
         Assert.True(result.IsSuccess);
-        _loggerMock.Verify(
-            logger => logger.Log(
-                LogLevel.Information,
-                It.IsAny<EventId>(),
-                It.Is<It.IsAnyType>((state, _) =>
-                    state.ToString() == "A dummy email was handled without external delivery."),
-                null,
-                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
-            Times.Once);
-        _loggerMock.VerifyNoOtherCalls();
     }
 
     private static EmailDto CreateEmail()
